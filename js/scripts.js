09b725db04b014bbff92b6e91a1bc5a30ca67f53
Vuyaa <st_im@hotmail.com>
2022-09-21 17:39:23 +0200

    First variable "pokemonList"
    Created the first variable in the new project "Pokedex" and assigning pokemons as objects in the array.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+let pokemonList = [{name: "Bulbasaur",type: ["grass", "poison"], height: 3},
+{name: "Snorlax", type:["normal", "fighting"], height: 8},
+{name: "Golduck", type:["water", "psychic"], height:5} 
+]
