2d0774f30a70827375306fab6dc3b3ba9634d966
Vuyaa <st_im@hotmail.com>
2022-09-23 11:35:19 +0200

    Format the scripts.js using formating guidlines
    Guidlines are from github

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 
+// Create an array of objects(pokemons) and assign atributes to them
 let pokemonList = [
-  {
+{
     name: " Bulbasaur",
     types: [" grass", " poison "],
     height: 3
@@ -16,14 +17,15 @@ let pokemonList = [
     height: 5
   },
 ];
-for (i = 0; i < pokemonList.length; i++)
 
+//For loop that adds a different comment depending on the pokemon size
+for (i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height < 5) {
     document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------He is tiny" + "<br>")
-  }
-else if (pokemonList[i].height >= 5 && pokemonList[i].height < 8) {
-  document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------Just a normal sized pokemon" + "<br>")
-} else {
-  document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------This one is enormous" + "<br>")
-};
-		
+  } else if (pokemonList[i].height >= 5 && pokemonList[i].height < 8) {
+    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------Just a normal sized pokemon" + "<br>")
+  } else {
+    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------This one is enormous" + "<br>")
+  };
+}
+
