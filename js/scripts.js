a419ee160fcb354f93d7512ee5c4412518e9cae9
Vuyaa <st_im@hotmail.com>
2022-09-26 12:48:54 +0200

    Created unordered list from pokemon data
    Removed <br> and puting all of the atributes within an object inside of the unordered list (ul) as an individual li (list item)

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,21 @@ let pokemonList = [
 ];
 
 //For loop that adds a different comment depending on the pokemon size
+document.write('<ul>')
 for (i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height < 5) {
-    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------He is tiny" + "<br>")
-  } else if (pokemonList[i].height >= 5 && pokemonList[i].height < 8) {
-    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------Just a normal sized pokemon" + "<br>")
-  } else {
-    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------This one is enormous" + "<br>")
+		document.write("<li>")
+    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------He is tiny")
+    document.write("</li>") 
+  }	else if (pokemonList[i].height >= 5 && pokemonList[i].height < 8) {
+    document.write("<li>")
+    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------Just a normal sized pokemon")
+    document.write("</li>")
+  } else  {
+		document.write("<li>")
+    document.write(pokemonList[i].name + ": " + pokemonList[i].types + pokemonList[i].height + " meter high <------This one is enormous")
+    document.write("</li>")
   };
 }
+document.write('</ul>')
 
