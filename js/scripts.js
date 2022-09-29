226e5b96e30aae2a16c0ca331063693505834ac6
Vuyaa <st_im@hotmail.com>
2022-09-29 10:09:23 +0200

    Add new pokemon using .add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,69 +1,77 @@
 //Create a variable to append our array to a IIFE to avoid accessing the global state
-let pokemonRepository = (function (){
-	
+let pokemonRepository = (function() {
 
-	// Create an array of objects(pokemons) and assign atributes to them
-	let pokemonList = [
-	{
-			name: " Bulbasaur",
-			types: [" grass", " poison "],
-			height: 3
-		},
-		{
-			name: " Snorlax",
-			types: [" normal", " fighting "],
-			height: 8
-		},
-		{
-			name: " Golduck",
-			types: [" water", " psychic "],
-			height: 5
-		},
-	]
-	
-	//IIFE part with functions getAll and add
-	function getAll() {
-		return pokemonList;
-	}
-	
-  function add(pokemon){
-  
-    if (typeof pokemon !== 'object'){
-      alert ('New pokemon has to be an object data type')
+
+  // Create an array of objects(pokemons) and assign atributes to them
+  let pokemonList = [{
+      name: " Bulbasaur",
+      types: [" grass", " poison "],
+      height: 3
+    },
+    {
+      name: " Snorlax",
+      types: [" normal", " fighting "],
+      height: 8
+    },
+    {
+      name: " Golduck",
+      types: [" water", " psychic "],
+      height: 5
+    },
+  ]
+
+  //IIFE part with functions getAll and add
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(pokemon) { 
+    if (typeof pokemon !== 'object') {
+      alert('New pokemon has to be an object data type')
     }
-    else pokemonList.push(pokemon)
-	}
-	
-return{
-	getAll:getAll,
-	add: add
-}
+		else if (!('name' in pokemon) ||  !('types' in pokemon) || !('height' in pokemon)){
+			alert('Object key is not specified')
+    } else {pokemonList.push(pokemon)
+   }
+  };
+
+  return {
+    getAll: getAll,
+    add: add
+  }
 })()
-	
-	document.write('<ul>')
+
+document.write('<ul>')
+//Add. function allows you to add new pokemon object
+  pokemonRepository.add({name: ' Jynx', types:[' Ice ', ' Psychic '], height: 5});
 //Acces to the pokemonList array inside the IIFE
   pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.add(pokemon)
 	
-//whole If-else statement has to be in its own {}curly parenthesis !!without semicolons!!
-//each action within the statements has to be in its own curly parentesis eg. if (//parameters) {}
-	   if (pokemon.height < 5) {
-     
-			document.write("<li>")
-		 document.write(pokemon.name + ": " + pokemon.types + pokemon.height + " meter high <------He is tiny")    
-		 document.write("</li>") 
-     
-		}	else if (pokemon.height >= 5 && pokemon.height < 8) {
-	
-      document.write("<li>")
-			document.write(pokemon.name + ": " + pokemon.types + pokemon.height + " meter high <------Just a normal sized pokemon")
-			document.write("</li>")
+  //whole If-else statement has to be in its own {}curly parenthesis !!without semicolons!!
+  //each action within the statements has to be in its own curly parentesis eg. if (//parameters) {}
+  if (pokemon.height < 5) {
 
-    } else  {
-			document.write("<li>")
-			document.write(pokemon.name + ": " + pokemon.types + pokemon.height + " meter high <------This one is enormous")
-		 document.write("</li>")
-		}
-		});
+    document.write("<li>")
+    document.write(pokemon.name + ": " + pokemon.types + pokemon.height + " meter high <------He is tiny")
+    document.write("</li>")
 
-	document.write('</ul>')
+  } else if (pokemon.height >= 5 && pokemon.height < 8) {
+
+    document.write("<li>")
+    document.write(pokemon.name + ": " + pokemon.types + pokemon.height + " meter high <------Just a normal sized pokemon")
+    document.write("</li>")
+
+  } else {
+    document.write("<li>")
+    document.write(pokemon.name + ": " + pokemon.types + pokemon.height + " meter high <------This one is enormous")
+    document.write("</li>")
+  }
+});
+
+document.write('</ul>')
+
+/*let pokemonAcces = pokemonRepository.getAll().filter(pokemonName => {
+  return pokemonName.name.includes('Golduck');
+});
+ console.log(pokemonAcces);*/
+ 
